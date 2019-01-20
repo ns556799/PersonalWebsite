20c01dd42bc4133648c339bef391cfa59ac10eac
Niv Suresh <nivsuresh06@gmail.com>
2019-01-20 10:26:25 +0000

    Typed component added

--- src/components/TypeWriter.js
+++ src/components/TypeWriter.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, {Component} from 'react'
+import Typed from 'typed.js'
 
-const TypeWriter = (props) => {
-    return(
-        <div>
-            <span className='typewriter'> $(<span className='first'>{props.first}</span>).<span className='last'>{props.last}</span>(function({props.function})</span>
+import '../css/TypeWriter.css'
+
+class TypeWriter extends Component {
+    componentDidMount() {
+        // If you want to pass more options as props, simply add
+      // your desired props to this destructuring assignment.
+      const { strings } = this.props;
+      // You can pass other options here, such as typing speed, back speed, etc.
+      const options = {
+          strings: strings,
+        typeSpeed: 50,
+        backSpeed: 50
+      };
+      // this.el refers to the <span> in the render() method
+      this.typed = new Typed(this.el, options);
+    }
+  
+    componentWillUnmount() {
+        // Make sure to destroy Typed instance on unmounting
+      // to prevent memory leaks
+      this.typed.destroy();
+    }
+  
+    render() {
+      return (
+        <div className="wrap">
+          <div className="type-wrap">
+            <span className="type-container"
+              style={{ whiteSpace: 'pre' }}
+              ref={(el) => { this.el = el; }}
+            />
+          </div>
         </div>
-    );
-}
+      );
+    }
+  }
+  
 
 export default TypeWriter
